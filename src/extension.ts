a483672fa8637dd98ac3b8189c37eb23847fa627
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-01-14 15:29:24 -0200

    Reading Omnisharp log file and wring sessiondata file.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,10 @@
 
 import * as vscode from 'vscode';
 import { listeners } from 'cluster';
+import { readFileSync, writeFile } from 'fs';
+import { SessionData } from './sessionData';
+
+const logFile = "C:\\Users\\EduardoAFontana\\vsdbg-ui.log";
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,8 +14,38 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     vscode.debug.onDidTerminateDebugSession((e: vscode.DebugSession) => {
-        console.log("Terminate: " + e);
-        vscode.window.showInformationMessage('Passed here!');
+        //console.log("Terminate: " + e);
+        //vscode.window.showInformationMessage('Passed here!');
+
+        var sessionData : SessionData = {
+            Identifier: "test identifier",
+            Label: "VSCode",
+            Description: "test description",
+            Purpose: "test purpose",
+            Started: new Date(),
+            Finished: new Date(),
+        
+            Breakpoints: [],
+            Events: [],
+            PathNodes: [],
+        
+            Task: {},
+            Developer: {},
+        };
+
+        let fileLines = readFileSync(logFile).toString().split('\n');
+
+        for (let line of fileLines) {
+            //criar objeto de sessão
+        }
+
+        writeFile("C:\\SwarmData\\VSCode\\session-20180114153066666.txt", JSON.stringify(sessionData, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            };
+            console.log("File has been created");
+        });
     });
 
     vscode.debug.onDidReceiveDebugSessionCustomEvent((e: vscode.DebugSessionCustomEvent) => {
