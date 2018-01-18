3e9f94a3468dc9d336f9ada0b51993c5df2e30b4
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-01-18 08:01:58 -0200

    Created invalidstartline and clearline.

--- src/sessionController.ts
+++ src/sessionController.ts
@@ -12,25 +12,34 @@ export class SessionController {
         FileController.processLog();
     }
 
-    public static processEntry(line : string) : void{
-        if(line.startsWith("->")){
+    public static processEntry(line : string) : void {
+        if(this.invalidStartLine(line))
+            return;
 
-            let strObjLine = line.replace("-> (C) ", "");
-            let objLine = JSON.parse(strObjLine);
-            
-            if(objLine.command == "launch"){
-                this.sessionData = SessionData.newSessionData();
-                this.sessionData.Identifier = objLine.arguments.__sessionId;
-                this.sessionData.Label = "VSCode";
-                this.sessionData.Description = "TODO";
-                this.sessionData.Purpose = "TODO";
-                this.sessionData.Started = new Date();
-            }
+        let strObjLine = this.clearLine(line);
+
+        let objLine = JSON.parse(strObjLine);
+
+        if(objLine.command == "launch"){
+            this.sessionData = SessionData.newSessionData();
+            this.sessionData.Identifier = objLine.arguments.__sessionId;
+            this.sessionData.Label = "VSCode";
+            this.sessionData.Description = "TODO";
+            this.sessionData.Purpose = "TODO";
+            this.sessionData.Started = new Date();
         }
 
         if(this.sessionData != undefined){
             FileController.writeOutput(JSON.stringify(this.sessionData, null, 2));
         }        
+    }
+
+    private static invalidStartLine(line : string) : boolean {
+        return !line.startsWith("->");
+    }
+
+    private static clearLine(line : string) : string {
+        return line.replace("-> (C) ", "");
     }
 
     /*
